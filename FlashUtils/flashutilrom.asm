852fdeb1a630689cc5a064bf6447c1d5e203b45f
Brendan Alford <brendanalford@eircom.net>
2015-05-30 23:28:07 +0100

    Detect if RAM is (somewhat) working before launching Flash utility

--- FlashUtils/flashutilrom.asm
+++ FlashUtils/flashutilrom.asm
@@ -24,6 +24,8 @@
 ; 	the OUT command would live (since the OUT (C), A switches page,
 ; 	the next fetched instruction will be from this file, not the Speccy ROM)
 
+	include "../defines.asm"
+	
         org 0
         jp copyflashutil
         block #38-$,0
@@ -50,7 +52,86 @@ flashutilend
 copyflashutil
  
 	di
-        ld hl, flashutil
+ 
+ ;	First check if we have any upper memory.
+ ;	This utility won't work on 16K machines.
+ 
+ 	ld hl, 0
+ 	ld (0x8000), hl
+ 	ld hl, (0x8000)
+ 	ld a, h
+ 	or l
+ 	jr z, docopy
+ 	
+ ;	Uh-oh, we weren't able to read and write 0000 to
+ ;	locations 32768/32769. Either we're running on a 
+ ;	16K Spectrum, or upper memory's faulty.
+ ;	(The flash utility could run in the lower 16K, but
+ ;	then there's no spare memory to copy ROM images to
+ ;	or from.)
+ 
+ 	xor a
+ 	ld hl, 0x4000
+ 	ld (hl), a
+ 	ld de, 0x4001
+ 	ld bc, 0x1800
+ 	ldir
+
+	ld ixl, 4
+	
+ramfail
+ 
+ 	ld a, 2
+ 	out (0xfe), a
+	
+	ld a, 0x10
+ 	ld hl, 0x5800
+ 	ld (hl), a
+ 	ld de, 0x5801
+ 	ld bc, 0x300
+ 	ldir
+ 	
+ 	ld hl, 0xffff
+ 
+ramfail_2
+ 
+ 	dec hl
+ 	ld a, h
+ 	or l
+ 	jr nz, ramfail_2
+ 	
+ 	ld a, 0
+ 	out (0xfe), a
+ 	
+ 	ld hl, 0x5800
+	ld (hl), a
+	ld de, 0x5801
+	ld bc, 0x300
+	ldir
+
+ 	ld hl, 0xffff
+
+ramfail_3
+
+	dec hl
+	ld a, h
+	or l
+	jr nz, ramfail_3
+	
+	dec ixl
+	ld a, ixl
+	cp 0
+	jr nz, ramfail
+
+     	xor a
+     	out (ROMPAGE_PORT), a   ; page in Speccy ROM
+     	ei
+     	ret
+
+
+docopy
+ 
+ 	ld hl, flashutil
         ld de, #E000   ; Decimal 57344
         ld bc, flashutilend-flashutil
         ldir
