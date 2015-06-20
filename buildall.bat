dca153a3cdbfb15c29a10eda6fa0202526dc1b3a
Brendan Alford <brendanalford@eircom.net>
2015-06-20 21:59:21 +0100

    Master build batch file

--- buildall.bat
+++ buildall.bat
@@ -1,9 +1,17 @@
 @echo off
-echo Building all...
+echo *****************
+echo Building Main ROM
+echo *****************
 call build.bat
 cd FlashUtils
+echo **********************
+echo Building FLASH Utility
+echo **********************
 call build.bat
 cd ..\TestTape
+echo *************************
+echo Building tape based tests
+echo *************************
 call build.bat
 cd ..
 echo All builds complete.
