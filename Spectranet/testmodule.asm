505133c2a4fee48fbcb50735611d53331e9cf2d9
Brendan Alford <brendan.alford@gmail.com>
2015-08-23 16:21:44 +0100

    Paint failed bits in attributes

--- Spectranet/testmodule.asm
+++ Spectranet/testmodule.asm
@@ -160,18 +160,145 @@ fail_next
 	ld a, '\n'
 	call PUTCHAR42
 	
-	jp exit
+	
+;
+;	Paint RAM FAIL message. This routine is borrowed from the 
+;	main diagnostics ROM.
+;	
+
+	ld hl, 0x5880
+	ld de, 0x5881
+	ld bc, 0x1ff
+	ld (hl), 9
+	ldir
+
+	ld hl, fail_ram_bitmap
+	ld de, 0x5880
+	ld b, 0x40
+
+fail_msg_loop
+
+	ld c, 8
+	ld a, (hl)
+
+fail_msg_byte
+
+	bit 7, a
+	jr z, fail_msg_next
+
+	ex de, hl
+	ld (hl), 0x7f
+	ex de, hl
+
+fail_msg_next
+
+	inc de
+	rla
+	dec c
+	jr nz, fail_msg_byte
+
+	inc hl
+	dec b
+	jr nz, fail_msg_loop
+	
+	
+;	Blank out the working RAM digits
+
+	ld hl, 0x5980
+	ld c, ixh
+
+	ld d, 8
+	
+fail_bits_outer_loop
+
+	ld b, 8
+	
+fail_bits_loop
+
+	bit 0, c
+	jr nz, fail_bits_ok
+	ld a, 8
+	ld (hl), a
+	inc hl
+	ld (hl), a
+	inc hl
+	ld (hl), a
+	inc hl
+	ld (hl), a
+	inc hl
+	jr fail_bits_next
+	
+fail_bits_ok
+
+	inc hl
+	inc hl
+	inc hl
+	inc hl
+	
+fail_bits_next
+
+	rrc c
+	djnz fail_bits_loop
+	
+	dec d
+	ld a, d
+	cp 0
+	jr nz, fail_bits_outer_loop
+	
+;	Wait for a key, then exit.
+
+	ld hl, str_pressanykey
+	call PRINT42
+	call GETKEY
+	ret
 
 tests_done
 	
 	ld hl, str_16kpassed
 	call PRINT42
 	
-exit
-	
-	ld hl, str_pressanykey
+	ld hl, str_furthertests
 	call PRINT42
 	call GETKEY
+	cp 'c'
+	ret nz
+	
+	ld hl, str_bootstrap
+	call PRINT42
+	call GETKEY
+	
+;	Load the tape based tests from remote host
+
+	
+	ld hl, str_bootstrap_host
+	ld de, 0x5b00
+	call GETHOSTBYNAME
+	jr c, bootstrap_error
+	ret
+	
+	ld c, 1 ;SOCK_STREAM
+	call SOCKET
+	jr c, bootstrap_error
+	ld (0x5b04), a
+	
+	ld de, 0x5b00
+	ld bc, 80
+	call CONNECT
+	jr c, bootstrap_error
+
+success
+
+	ld hl, str_bootstrap_success
+	call PRINT42
+	
+success_2
+
+	jr success_2
+	
+bootstrap_error
+
+	ld hl, str_bootstrap_error
+	call PRINT42
 	ret
 	
 initroutine
@@ -200,17 +327,66 @@ press_r_loop
 
 	ld hl, str_not_testing
 	call PRINT42
+	call test_cmd
 	ret
 
 run_tests
 
 	call modulecall
 	ret
+
+;
+;	Implementation of the '.test' command
+;	
+test_cmd
+
+	ld hl, parsetable
+	call ADDBASICEXT
+	ret nc
+	ld hl, str_cmd_fail
+	call PRINT42
+	ret
 	
+	
+parsetable
+
+	defb 0x0b
+	defw test_cmd_string
+	defb 0xff
+	defw modulecall
+		
+test_cmd_string
+
+	defb "%test", 0
+	
+fail_ram_bitmap
+
+	defb %00000000, %00000000, %00000000, %00000000
+	defb %01100010, %01010000, %11100100, %11101000
+	defb %01010101, %01110000, %10001010, %01001000
+	defb %01010101, %01110000, %11001010, %01001000
+	defb %01100111, %01010000, %10001110, %01001000
+	defb %01010101, %01010000, %10001010, %01001000
+	defb %01010101, %01010000, %10001010, %11101110
+	defb %00000000, %00000000, %00000000, %00000000
+	
+	defb %00000000, %00000000, %00000000, %00000000
+	defb %01000100, %01001110, %00101110, %01101110
+	defb %10101100, %10100010, %10101000, %10000010
+	defb %10100100, %00100100, %10100100, %11000100
+	defb %10100100, %01000010, %11100010, %10100100
+	defb %10100100, %10001010, %00101010, %10101000
+	defb %01001110, %11100100, %00100100, %01001000
+	defb %00000000, %00000000, %00000000, %00000000	
+
 ;
 ;	Text strings
 ;
 
+str_cmd_fail
+
+	defb "Failed to add BASIC extension\n", 0
+	
 str_identity
 
 	defb "ZX-Diagnostics ", VERSION, 0 
@@ -234,6 +410,30 @@ str_16kfailed
 str_failedbits
 
 	defb "Failed bit locations: ", 0
+	
+str_furthertests
+
+	defb "Press C to continue tests, or any other\nkey to exit.\n", 0
+	
+str_bootstrap
+
+	defb "Requesting test code from server...\n", 0
+	
+str_bootstrap_host
+
+	defb "www.bennyalford.com", 0
+	
+str_bootstrap_httpget
+
+	defb "GET /testrammain.bin HTTP/1.0\n", 0
+	
+str_bootstrap_error
+
+	defb "Could not retrieve test code.\n", 0
+	
+str_bootstrap_success
+
+	defb "Test code loaded, starting...\n", 0
 	
 str_pressanykey
 
