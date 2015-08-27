74e30a3f665b90ed1d9cb59b47aa7026ef3e43dc
ZXGuesser <alistair@zxnet.co.uk>
2015-08-27 22:00:03 +0100

    rename testmodule2 -> testmodule1

--- Spectranet/build.bat
+++ Spectranet/build.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Building Spectranet Test ROM module...
-sjasmplus testmodule.sjasm
+sjasmplus testmodule1.sjasm
 sjasmplus testmodule2.sjasm
 echo make installers...
-call make-installer.bat testmodule.module
+call make-installer.bat testmodule1.module
 call make-installer.bat testmodule2.module
 echo Done!
