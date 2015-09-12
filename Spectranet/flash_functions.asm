2caf848b56f02f7c6c369db6661a9435ccb98549
Brendan Alford <brendan.alford@gmail.com>
2015-09-12 01:28:58 +0100

    Unified installer

---
+++ Spectranet/flash_functions.asm
@@ -0,0 +1,316 @@
+;
+;	ZX Diagnostics - fixing ZX Spectrums in the 21st Century
+;	https://github.com/brendanalford/zx-diagnostics
+;
+;	Original code by Dylan Smith
+;	Modifications and 128K support by Brendan Alford
+;
+;	This code is free software; you can redistribute it and/or
+;	modify it under the terms of the GNU Lesser General Public
+;	License as published by the Free Software Foundation;
+;	version 2.1 of the License.
+;
+;	This code is distributed in the hope that it will be useful,
+;	but WITHOUT ANY WARRANTY; without even the implied warranty of
+;	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;	Lesser General Public License for more details.
+;
+;	flash_functions.asm
+;	
+;	Flash utilities for Spectranet test modules.
+;	Heavily borrowed from ZXGuesser and Winston's code.
+;
+
+;
+;	Writes a given ROM page. This is entered as follows:
+;	v_modaddr contains the start address in main RAM of the module to write
+;	v_modindex contains its desired index
+;	This routine loops through all sectors (32 / 4 = 8) and
+;	detects if it needs to write a module to this specific sector.
+;	If so, it backs up the sectors to Spectranet SRAM (replacing the target)
+;	page as it goes, then erases the sector, and rewrites the lot.
+;	I imagine I'll be using the restore functionality a lot :)
+;
+write_rom_page
+
+;	Page index is held in B
+	ld b, MIN_PAGE
+
+write_page_loop
+
+	ld a, (v_modindex)
+	cp b
+	jr nz, write_page_next
+	
+;	We need to write to this page within this sector.
+;	Step B back to B mod 4 for a full sector,
+;	then back it up page by page to SRAM, except if 
+; 	the current page matches our target, then take it instead.
+
+	push bc
+	ld a, b
+	and 0xfc
+	ld b, a
+	
+;	Start with page 0xDC of SRAM
+
+	ld c, 0xdc
+
+copy_to_sram_loop
+
+	ld a, c
+	push bc
+	call SETPAGEB
+	pop bc
+	ld a, b
+	push bc
+	call SETPAGEA
+	pop bc
+	
+;	Is our page current?
+
+	ld a, (v_modindex)
+	cp b
+	jr nz, copy_rom_to_sram
+	
+;	Yes, copy the 4K block at v_modaddr to SRAM
+
+	push hl
+	push de
+	push bc
+	ld hl, (v_modaddr)
+	ld de, 0x2000
+	ld bc, 0x1000
+	ldir
+	pop bc
+	pop de
+	pop hl
+	jr copy_to_sram_next
+
+;	No, just copy this page from Flash to SRAM
+
+copy_rom_to_sram
+	
+	push hl
+	push de
+	push bc
+	ld hl, 0x1000
+	ld de, 0x2000
+	ld bc, 0x1000
+	ldir
+	pop bc
+	pop de
+	pop hl
+	
+copy_to_sram_next
+
+	inc b
+	inc c
+	ld a, c
+	cp 0xe0
+	jr nz, copy_to_sram_loop
+	
+;	At this point, we have a full sector backed up to SRAM.
+;	Now erase its target sector.
+
+	ld a, 2
+	out (0xfe), a
+	
+	pop bc
+	ld a, b
+	and 0xfc
+
+	push bc
+	call F_FlashEraseSector
+	pop bc
+	jr c, write_page_erase_error
+
+	ld a, b
+	call F_writesector
+	jr c, write_page_write_error
+	
+	ld a, 1
+	out (0xfe), a
+	
+;	If we've written a sector, our work here is done.
+	ret
+	
+write_page_next
+
+	inc b
+	ld a, b
+	cp MAX_PAGE
+	jr nz, write_page_loop
+	ret
+	
+write_page_erase_error
+
+	ld hl, str_eraseerror
+	jr write_page_giveup
+	
+write_page_write_error
+	
+	ld hl, str_writeerror
+	
+write_page_giveup
+		
+	ld a, 1
+	out (0xfe), a
+	CALL PRINT42
+	scf
+	ret
+	
+;---------------------------------------------------------------------------
+; F_FlashEraseSector
+; Simple flash writer for the Am29F010 (and probably any 1 megabit flash
+; with 16kbyte sectors)
+;
+; Parameters: A = page to erase (based on 4k Spectranet pages, but
+; erases a 16k sector)
+; Carry flag is set if an error occurs.
+F_FlashEraseSector: 
+
+        ; Page in the appropriate sector first 4k into page area B.
+        ; Page to start the erase from is in A.
+        call SETPAGEB 	; page into page area B
+
+        ld a, 0xAA      ; unlock code 1
+        ld (0x555), a   ; unlock addr 1
+        ld a, 0x55      ; unlock code 2
+        ld (0x2AA), a   ; unlock addr 2
+        ld a, 0x80      ; erase cmd 1
+        ld (0x555), a   ; erase cmd addr 1
+        ld a, 0xAA      ; erase cmd 2
+        ld (0x555), a   ; erase cmd addr 2
+        ld a, 0x55      ; erase cmd 3
+        ld (0x2AA), a   ; erase cmd addr 3
+        ld a, 0x30      ; erase cmd 4
+        ld (0x2000), a  ; erase sector address
+
+        ld hl, 0x2000
+.wait1: 
+        bit 7, (hl)     ; test DQ7 - should be 1 when complete
+        jr nz,  .complete1
+        bit 5, (hl)     ; test DQ5 - should be 1 to continue
+        jr z,  .wait1
+        bit 7, (hl)     ; test DQ7 again
+        jr z,  .borked1
+
+.complete1: 
+        or 0            ; clear carry flag
+        ret
+
+.borked1: 
+        scf             ; carry flag = error
+        ret
+
+;---------------------------------------------------------------------------
+; F_FlashWriteByte
+; Writes a single byte to the flash memory.
+; Parameters: DE = address to write
+;              A = byte to write
+; On return, carry flag set = error
+; Page the appropriate flash area into one of the paging areas to write to
+; it, and the address should be in that address space.
+F_FlashWriteByte: 
+        push bc
+        ld c, a         ; save A
+
+        ld a, 0xAA      ; unlock 1
+        ld (0x555), a   ; unlock address 1
+        ld a, 0x55      ; unlock 2
+        ld (0x2AA), a   ; unlock address 2
+        ld a, 0xA0      ; Program
+        ld (0x555), a   ; Program address
+        ld a, c         ; retrieve A
+        ld (de), a      ; program it
+
+		and 0x7
+		out (0xfe), a
+.wait3: 
+        ld a, (de)      ; read programmed address
+        ld b, a         ; save status
+        xor c           
+        bit 7, a        ; If bit 7 = 0 then bit 7 = data        
+        jr z,  .byteComplete3
+
+        bit 5, b        ; test DQ5
+        jr z,  .wait3
+
+        ld a, (de)      ; read programmed address
+        xor c           
+        bit 7, a        ; Does DQ7 = programmed data? 0 if true
+        jr nz,  .borked3
+
+.byteComplete3: 
+        pop bc
+        or 0            ; clear carry flag
+        ret
+
+.borked3: 
+        pop bc
+        scf             ; error = set carry flag
+        ret
+
+;---------------------------------------------------------------------------
+; F_FlashWriteBlock
+; Copies a block of memory to flash. The flash should be mapped into
+; page area B.
+; Parameters: HL = source start address
+;             DE = destination start address
+;             BC = number of bytes to copy
+; On error, the carry flag is set.
+F_FlashWriteBlock: 
+        ld a, (hl)      ; get byte to write
+        call F_FlashWriteByte
+        ret c           ; on error, return immediately
+        inc hl          ; point at next source address
+        inc de          ; point at next destination address
+        dec bc          ; decrement byte count
+        ld a, b
+        or c            ; is it zero?
+        jr nz, F_FlashWriteBlock
+        ret
+
+		
+;---------------------------------------------------------------------------
+; F_writesector
+; Writes 4 pages from the last 4 pages of RAM to flash, starting at the
+; page specified in A
+F_writesector: 
+        ex af, af'      ; swap with alternate set
+        ld a, 0xDC      ; RAM page 0xDC
+        ld b, 4         ; number of pages
+.loop4: 
+        push bc
+        call SETPAGEA ; Page into area A
+        inc a           ; next page
+        ex af, af'      ; get flash page to program
+        call SETPAGEB
+        inc a           ; next page
+        ex af, af'      ; back to ram page for next iteration
+        ld hl, 0x1000
+        ld de, 0x2000
+        ld bc, 0x1000
+        push af
+        call F_FlashWriteBlock
+        jr c,  .failed4 ; restore stack and exit
+        pop af
+        pop bc
+        djnz  .loop4    ; next page
+        ret
+.failed4:               ; restore stack, set carry flag
+        pop af
+        pop bc
+        scf
+        ret
+		
+str_eraseerror
+
+	defb "Error erasing Flash sector, aborting.\n",0
+	
+str_writeerror
+
+	defb "Error writing Flash sector, aborting.\n",0
+	
+
---
+++ Spectranet/installer.asm
@@ -0,0 +1,362 @@
+;
+;	ZX Diagnostics - fixing ZX Spectrums in the 21st Century
+;	https://github.com/brendanalford/zx-diagnostics
+;
+;	Original code by Dylan Smith
+;	Modifications and 128K support by Brendan Alford
+;
+;	This code is free software; you can redistribute it and/or
+;	modify it under the terms of the GNU Lesser General Public
+;	License as published by the Free Software Foundation;
+;	version 2.1 of the License.
+;
+;	This code is distributed in the hope that it will be useful,
+;	but WITHOUT ANY WARRANTY; without even the implied warranty of
+;	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;	Lesser General Public License for more details.
+;
+;	installer.asm
+;	
+;	Installer for Spectranet test modules.
+;
+
+;	Min/Maximum Spectranet ROM pages we can touch.
+
+	define MIN_PAGE 0x04
+	define MAX_PAGE 0x1F
+	
+	define PAYLOAD_OFFSET 0x500
+	
+	include "spectranet.asm"
+	include "..\version.asm"
+	
+	org 0x8000
+	
+	call PAGEIN
+	call CLEAR42
+	
+	ld hl, str_title
+	call PRINT42
+	
+	call scan_flash
+	ld a, (v_ok)
+	cp 0
+	jp nz, exit
+
+
+;	Give the user one last chance to bail out
+
+	ld hl, str_confirm
+	call PRINT42
+	call GETKEY
+	cp 'p'
+	jp nz, exit
+	
+	ld hl, v_freepages
+
+checkmodule1
+
+	ld a, (v_module1page)
+	cp 0xff
+	jr nz, module1found
+	
+	ld a, (hl)
+	cp 0xff
+	jp z, no_free_pages
+	inc hl
+	push hl
+	push af
+	ld (v_module1page), a
+	ld hl, str_new_module1
+	call PRINT42
+	pop af
+	ld hl, v_workspace
+	call ITOH8
+	ld hl, v_workspace
+	call PRINT42
+	ld hl, str_found_module_end
+	call PRINT42
+	pop hl	
+	jr checkmodule2
+	
+module1found
+
+	push hl
+	ld hl, str_found_module1
+	call PRINT42
+	ld hl, v_workspace
+	ld a, (v_module1page)
+	call ITOH8
+	ld hl, v_workspace
+	call PRINT42
+	ld hl, str_found_module_end
+	call PRINT42
+	pop hl
+
+checkmodule2
+
+	ld a, (v_module2page)
+	cp 0xff
+	jr nz, module2found
+	
+	ld a, (hl)
+	cp 0xff
+	jr z, no_free_pages
+	inc hl
+	push hl
+	push af
+	ld (v_module2page), a
+	ld hl, str_new_module2
+	call PRINT42
+	pop af
+	ld hl, v_workspace
+	call ITOH8
+	ld hl, v_workspace
+	call PRINT42
+	ld hl, str_found_module_end
+	call PRINT42
+	pop hl	
+	jr continue
+
+module2found
+
+	push hl
+	ld hl, str_found_module2
+	call PRINT42
+	ld hl, v_workspace
+	ld a, (v_module2page)
+	call ITOH8
+	ld hl, v_workspace
+	call PRINT42
+	ld hl, str_found_module_end
+	call PRINT42
+	pop hl	
+
+;	Start erasing/programming pages according to the contents of
+;	v_module1page and v_module2page.
+
+continue
+
+	ld hl, str_writing1
+	call PRINT42
+	ld a, (v_module1page)
+	ld (v_modindex), a
+	ld hl, 0x8000 + PAYLOAD_OFFSET
+	ld (v_modaddr), hl
+	di
+	call write_rom_page
+	ei
+	jr c, exit
+
+	ld hl, str_writing2
+	call PRINT42
+	ld a, (v_module2page)
+	ld (v_modindex), a
+	ld hl, 0x9000 + PAYLOAD_OFFSET
+	ld (v_modaddr), hl
+	di
+	call write_rom_page
+	ei
+	jr c, exit
+	
+complete
+
+	ld hl, str_done
+	call PRINT42
+	jr exit
+	
+no_free_pages
+
+	ld hl, str_no_free_pages
+	call PRINT42
+	
+exit
+
+	call PAGEOUT
+	ret
+	
+;
+;	Scans Flash ROM memory looking for module 1 and module 2 of 
+;	the diagnostics, and also looks for any free pages should
+;	they be needed if the diags aren't already installed
+;
+scan_flash
+
+	ld hl, 0xffff
+	exx
+	ld de, v_freepages
+	exx
+	ld (v_module1page), hl
+	ld (v_freepages), hl
+	ld a, 0xff
+	ld (v_ok), a
+	ld a, MIN_PAGE
+	
+scan_flash_loop
+	
+	push af
+	
+;	Page in the ROM and see if it's blank (first byte 0xff)
+
+	call SETPAGEB
+	ld a, (0x2000)
+	cp 0xFF
+	jr z, scan_flash_blank
+	
+;	Not blank, see if the identity string matches module 1
+
+match_mod1
+
+	ld hl, (0x200E)
+	ld de, str_identity1
+	call string_hl_startswith_de
+	jr nz, match_mod2
+
+;	Found module 1, check if we already saw one
+	ld a, (v_module1page)
+	cp 0xff
+	jr nz, scan_flash_duplicate
+	
+	pop af
+	ld (v_module1page), a
+	push af
+	jr scan_flash_next
+
+;	No match, see if the identity string matches module 2
+	
+match_mod2
+
+	ld hl, (0x200E)
+	ld de, str_identity2
+	call string_hl_startswith_de
+	jr nz, scan_flash_next
+	
+;	Found module 2, check if we already saw one
+	ld a, (v_module2page)
+	cp 0xff
+	jr nz, scan_flash_duplicate
+	
+	pop af
+	ld (v_module2page), a
+	push af
+	jr scan_flash_next
+	
+;	ROM page is blank, mark it as available if we need to install from scratch
+
+scan_flash_blank
+
+	exx
+	pop af
+	ld (de), a
+	inc de
+	push af
+	exx
+
+;	Keep checking until last page is reached
+;	Page 1F is reserved so exit when that's hit.
+
+scan_flash_next
+
+	pop af
+	inc a
+	cp MAX_PAGE
+	jr nz, scan_flash_loop
+	
+	xor a
+	ld (v_ok), a
+	
+	ret
+
+;	More than one of a particular module was found.
+;	Tell the user to clean up their mess before 
+;	rerunning.
+scan_flash_duplicate
+	
+	pop af
+	ld hl, str_duplicate
+	call PRINT42
+	ret
+	
+string_hl_startswith_de
+
+	ld a, (de)
+	cp (hl)
+	ret nz
+	inc de
+	inc hl
+	ld a, (de)
+	cp 0
+	ret z
+
+	jr string_hl_startswith_de
+	
+str_title
+
+	defb "ZX Diagnostics ", VERSION, " Spectranet Installer\n\n", 0
+	
+str_confirm
+
+	defb "Press P to install, any other key aborts\n\n", 0
+	
+str_identity1
+
+	defb "ZX Diagnostics Module 1", 0 
+
+str_identity2
+
+	defb "ZX Diagnostics Module 2", 0 
+	
+str_found_module1
+
+	defb "Found module 1 in slot: [", 0
+	
+str_found_module2
+
+	defb "Found module 2 in slot: [", 0
+	
+str_new_module1
+	
+	defb "Module 1: new, target slot: [", 0
+
+str_new_module2
+
+	defb "Module 2: new, target slot: [", 0
+
+str_found_module_end
+
+	defb "]\n", 0
+
+str_writing1
+
+	defb "Writing module 1...\n", 0
+
+str_writing2
+
+	defb "Writing module 2...\n", 0
+
+str_no_free_pages
+
+	defb "ERROR: Not enough free pages to complete\nthe install, exiting.", 0 
+	
+str_duplicate
+
+	defb "ERROR: Duplicate modules found. Delete\nthese duplicates via the ROM manager\nbefore retrying. Exiting.\n", 0
+
+str_done
+
+	defb "\nComplete.", 0
+	
+	include "flash_functions.asm"
+	
+	BLOCK (0x8000 + PAYLOAD_OFFSET)-$, 0x00
+
+	incbin "testmodule1.module"
+	incbin "testmodule2.module"
+	
+v_module1page	equ 0xff00		; Address of module 1 in flash or 0xFF if not found
+v_module2page	equ 0xff01		; Address of module 2 in flash of 0xFF if not found
+v_ok			equ 0xff02		; Module check pass if 0
+v_modaddr		equ 0xff03		; Address of module to copy
+v_modindex		equ 0xff05		; ROM index of module to write to
+v_freepages		equ 0xff06		; Indexes of first free page or 0xFF if non
+v_workspace		equ 0xff80		; General workspace
