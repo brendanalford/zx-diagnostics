2a05de76e91e48cbf61c4261b913ff912efe69d4
Brendan Alford <brendan.alford@gmail.com>
2016-12-20 21:15:55 +0000

    Remove relative path reference to bin2tap.

--- TestTape/build.bat
+++ TestTape/build.bat
@@ -1,12 +1,12 @@
 @echo off
 echo Building main test...
-sjasmplus testrammain.sjasm  --lst=testrammain.lst --lstlab 
+sjasmplus testrammain.sjasm  --lst=testrammain.lst --lstlab
 if %errorlevel% neq 0 goto :end
 echo Building bootstrap...
 sjasmplus testram.sjasm  --lst=testram.lst --lstlab
 if %errorlevel% neq 0 goto :end
 echo Creating tape image...
-..\bin2tap -b -a 24500 -c 24499 -r 24500 testram.bin
+bin2tap -b -a 24500 -c 24499 -r 24500 testram.bin
 echo Build complete.
 copy /y testram.bin bin
 copy /y testram.tap bin
