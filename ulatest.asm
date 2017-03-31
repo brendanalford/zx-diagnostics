1aad6e633744b15a9aafc33d2dcb5412e2f2b27c
brendanalford <brendanalford@eircom.net>
2017-03-31 17:59:28 +0100

    Remove ULA easter egg

--- ulatest.asm
+++ ulatest.asm
@@ -26,23 +26,7 @@ ulatest
 	ld sp, sys_stack
 	call initialize
 
-;	Keypress check
-
-	exx
-	ld l, 0
-	ld bc, 0xbffe
-	in a, (c)
-	bit 1, a
-	jr nz, write_shadow_screen
-	ld bc, 0xfdfe
-	in a, (c)
-	bit 0, a
-	jr nz, write_shadow_screen
-	ld l, 0xff
-
 write_shadow_screen
-
-	exx
 
 ;	Write some data to Screen 1.
 ;	This'll just write to the C000 area on a 48K machine,
@@ -62,7 +46,7 @@ write_shadow_screen
 	ld a, 0x20
 	call blankmem
 
-	xor a 
+	xor a
 	call pagein
 
 ;	Detect frame length. Once HALT is issued, we start counting until the
@@ -219,35 +203,6 @@ ula_print_floatbus_type
 	call print
 	ld hl, str_ulaexit
 	call print
-
-	exx
-	ld a, l
-	exx
-
-	cp 0xff
-	jr nz, do_footer
-
-	ld hl, str_message
-	ld de, 0x7a00
-
-.loop_1
-
-	ld a, (hl)
-	dec a
-	ld (de), a
-	inc de
-	inc hl
-	cp 0xff
-	jr nz, .loop_1
-
-	dec de
-	xor a
-	ld (de), a
-	ld hl, 0x7a00
-	call print
-
-do_footer
-
 	call print_footer
 	exx
 
@@ -841,7 +796,3 @@ str_fb_detected
 str_fb_absent
 
 	defb "absent", 0
-
-str_message
-
-	defb 24, 21, 17, "VVVVVVVVVVVVMMMMMMMMMMBBBBBBBBBBB\"\"\"\"\"", 0
