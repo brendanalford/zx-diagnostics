ffb8cdfba3aac15b5d300cbe4d24860ae8c0673c
brendanalford <brendanalford@eircom.net>
2017-05-11 15:08:26 +0100

    Change testram loader style

--- TestTape/build.bat
+++ TestTape/build.bat
@@ -6,7 +6,8 @@ echo Building bootstrap...
 sjasmplus testram.sjasm  --lst=testram.lst --lstlab
 if %errorlevel% neq 0 goto :end
 echo Creating tape image...
-bin2tap -b -a 24500 -c 24499 -r 24500 testram.bin
+bin2tap -a 24500 -o testram_main.tap testram.bin
+copy /b testram_loader.tap + testram_main.tap testram.tap
 echo Build complete.
 copy /y testram.bin bin
 copy /y testram.tap bin
